4119001617b37621ad0a02cfc0b26ee4075951fa
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-12-22 23:54:39 -0500

    Fundamentos: primitvos

---
+++ JAVASCRIPT/2. FUNDAMENTOS/assets/js/primitivos.js
@@ -0,0 +1,46 @@
+/* STRINGS */
+
+let nombre = 'Peter Parker';
+console.log( nombre );
+
+nombre = 'Ben Parker'
+console.log( nombre );
+
+nombre = "Tía May";
+nombre = `Tía May`;
+
+console.log(typeof nombre);
+
+nombre = 123;
+
+console.log(typeof nombre);
+
+/* BOOLEANOS */
+
+let isMarvel = true;
+
+console.log(isMarvel);
+
+/* NUMBERS */
+
+let edad = 33.01;
+console.log(edad);
+console.log(typeof edad);
+
+/* UNDEFINED */
+
+let superPoder;
+console.log(typeof superPoder);
+
+/* NULL */
+
+let soyNull = null;
+console.log(typeof soyNull);
+
+/* SYMBOL */
+let symbol2 = Symbol('a');
+let symbol1 = Symbol('a');
+
+console.log(typeof symbol1);
+
+console.log( symbol1 === symbol2);
