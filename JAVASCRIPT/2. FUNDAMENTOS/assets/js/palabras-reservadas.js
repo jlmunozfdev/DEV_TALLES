6756f3a5782ce7f86ed6b9f4cdde94192331cc70
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-12-23 00:18:24 -0500

    Fundamentos: palabras-reservadas

---
+++ JAVASCRIPT/2. FUNDAMENTOS/assets/js/palabras-reservadas.js
@@ -0,0 +1,21 @@
+
+
+let constante = 123;
+
+// const
+
+//if
+
+//try
+
+//while
+//for()
+
+//enum
+
+/* Nombres de variables */
+
+let objetos$ = 123;
+let objetos123 = 123;
+let precio_total = 123;
+let _abc = 123;
