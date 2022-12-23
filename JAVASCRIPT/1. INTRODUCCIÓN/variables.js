c7c5c99db99fb15a5cfd2cf67b52d762cf1f0105
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-12-22 22:21:45 -0500

    Second Commit: variables

---
+++ JAVASCRIPT/1. INTRODUCCIÓN/variables.js
@@ -0,0 +1,19 @@
+/* VARIABLES */
+
+let a = 10;
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+    
+const saludo = c + d; 
+
+/* console.log('%c Mis Variables', 'color:red; font-weight: bold');
+console.log( {a} );
+console.log( {b} );
+console.log( {c} );*/
+
+
+// console.table({ a, b, c, d, x });
+
+console.log(saludo);
