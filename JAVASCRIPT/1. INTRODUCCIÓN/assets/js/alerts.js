e9b988c43c8d9357c2c9c8cab87122d9c4ee86ab
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-12-22 23:25:27 -0500

    4to commit: alerts

---
+++ JAVASCRIPT/1. INTRODUCCIÓN/assets/js/alerts.js
@@ -0,0 +1,13 @@
+/* ALERT */
+
+// alert('Hola Mundo');
+
+/* PROMPT */
+
+/* let nombre = prompt('¿Cuál es tu nombre?', 'Sin nombre')
+console.log( nombre ); */
+
+
+let seleccion = confirm('¿Esta seguro de borrar esto?');
+
+console.log(seleccion);
