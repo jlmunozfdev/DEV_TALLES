a9c29ece32ac61d229c3c1b0f9c89ec196670688
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-12-23 21:47:44 -0500

    Ciclos: switch

---
+++ JAVASCRIPT/3. CICLOS_ESTRUCTURAS DE CONTROL/assets/js/switch.js
@@ -0,0 +1,33 @@
+
+
+const dia = 5;
+
+switch ( dia ) {
+
+    case 0: 
+    console.log('Domingo');
+    break;
+    case 1: 
+    console.log('Lunes');
+    break;
+    case 2: 
+    console.log('Martes');
+    break;
+    case 3: 
+    console.log('Miércoles');
+    break;
+    case 4: 
+    console.log('Jueves');
+    break;
+    case 5: 
+    console.log('Viernes');
+    break;
+    case 5: 
+    console.log('Sábado');
+    break;
+    default: 'Día no existe';
+    break;
+
+}
+
+console.log(dia);
