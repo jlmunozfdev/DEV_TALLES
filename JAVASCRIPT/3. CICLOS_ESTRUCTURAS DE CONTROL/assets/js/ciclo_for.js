3ec1e9be7db303e03568307bc5fec447e69a0062
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-12-23 22:10:31 -0500

    Ciclos: while_do_whil

---
+++ JAVASCRIPT/3. CICLOS_ESTRUCTURAS DE CONTROL/assets/js/ciclo_for.js
@@ -0,0 +1,21 @@
+/* CICLO FOR */
+
+const heroes = ['Batman', 'Superman', 'Mujer Maravilla', 'Acuaman'];
+
+console.warn('Ciclo For Tradicional');
+
+
+for ( let i = 0; i < heroes.length; i++){
+    console.log(heroes[i]);
+}
+
+console.warn('Ciclo For in');
+
+for ( let j in heroes ) {
+    console.log( heroes[j] );
+}
+
+console.warn('Ciclo For of'); //Se suele nombrar a las variables en singular
+for ( let hero of heroes ) {
+    console.log( hero );
+}
