cd374e70c79552cacf4f12c439a84faa97571a49
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-12-27 14:45:27 -0500

    BlackJack_Vite: Update 27-DEC refactorized

---
+++ JAVASCRIPT/BlackJack_Vite/src/blackjack/usecases/index.js
@@ -0,0 +1,7 @@
+
+export { crearDeck } from './crear-deck';
+export { crearCartaHTML } from './crear-carta-html';
+export { pedirCarta } from './pedir-carta';
+export { valorCarta } from './valor-carta';
+
+export { turnoComputadora } from './turno-computadora';
