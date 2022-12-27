175c64a5bba619241b825e9446cf477d4aec4a79
jlmunozfdev <jlmunozf.dev@gmail.com>
2022-12-26 21:51:29 -0500

    Clases: propiedades_gets_mestaticos

---
+++ JAVASCRIPT/5. CLASES/assets/js/propiedades_gets_mestaticos.js
@@ -0,0 +1,71 @@
+class Persona {
+
+   static _conteo = 0;
+   static get conteo() {
+    return Persona._conteo + ' instancias';
+   }
+
+   static mensaje() {
+    console.log(this.nombre);
+    console.log('Hola a todos soy un método estático');
+   }
+
+    nombre = '';
+    codigo = '';
+    frase = '';
+    comida = '';
+
+
+    constructor( nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase' ) {
+
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.frase = frase;
+
+        Persona._conteo++;
+    }
+
+    set setComidaFavorita( comida ) {
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita() {
+        return `La comida favorita de ${ this.nombre } es ${ this.comida}`
+    }
+   //Métodos
+   quienSoy() {
+    console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`);
+    }
+
+    miFrase() {
+    this.quienSoy();
+    console.log(`${this.codigo} dice: ${this.frase}`);
+    }
+
+}
+
+const spiderman = new Persona('Peter Parker', 'Spiderman', 'Soy tu amigable vecino Spiderman');
+const iroman = new Persona('Tony Stark', 'Iroman', 'Yo soy Iroman');
+
+
+// console.log(iroman);
+
+spiderman.miFrase();
+// iroman.miFrase();
+
+spiderman.setComidaFavorita = 'El pie de cereza de la tía May';
+//spiderman.comida = 'Duende Verde';
+
+
+// console.log(spiderman.getComidaFavorita);
+
+// console.log(spiderman);
+
+// Persona._conteo = 2;
+console.log('Conteo stático', Persona._conteo);
+console.log(Persona.conteo);
+Persona.mensaje();
+
+Persona.propiedadExterna = 'Hola Mundo';
+console.log(Persona.propiedadExterna);
+console.log(Persona);
