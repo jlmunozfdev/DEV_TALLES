e2c6169529347a9a9fbc1274294621a16d5c17c9
jlmunozfdev <jlmunozf.dev@gmail.com>
2023-01-02 13:51:28 -0500

    Update 7. Advanced concepts

--- JAVASCRIPT/7. ADVANCED_CONCEPTS/src/concepts/03-promises.js
+++ JAVASCRIPT/7. ADVANCED_CONCEPTS/src/concepts/03-promises.js
@@ -1,4 +1,4 @@
-import { heroes } from "../data/heroes"
+import { heroes } from "../data/heroes";
 /**
  * 
  * @param {HTMLDivElement} element 
@@ -11,20 +11,53 @@ export const promiseComponent = ( element ) => {
     element.innerHTML = hero.name;
   }
 
+
+  const renderTwoHeroes = (hero1, hero2) => {
+    element.innerHTML = `
+        <h3>${ hero1.name }</h3>    
+        <h3>${ hero2.name }</h3>    
+    `;
+  }
+
   const renderError = ( error ) => {
     element.innerHTML = `
             <h1>Error:</h1>
-            <h3>${error}</h3>`;
+            <h3>${error}</h3>
+            `;
   }
 
-  const id1 = '5d86371f1efebc31def272e2'
+  const id1 = '5d86371f1efebc31def272e2';
+  const id2 = '5d86371f25a058e5b1c8a65e';
 
-  findHero( id1 )
-        .then( renderHero )
-        .catch( renderError );
-
+  Promise.all([
+    findHero(id1),
+    findHero(id2),
+  ])
+  .then( ([hero1, hero2]) => renderTwoHeroes( hero1, hero2) )
+  .catch( renderError );
 }
+      //! Forma 2
+    // let hero1;
+    // findHero(id1)
+    //     .then( hero => {
+    //         hero1 = hero;
+    //         return findHero(id2);
+    //     }).then( hero2 => {
+    //         renderTwoHeroes( hero1, hero2 );
+    //     })
+    //     .catch( renderError );
 
+    //! Forma 1
+    // findHero( id1 )
+    //     .then( (hero1) => {
+  
+    //         findHero( id2 )
+    //             .then( hero2 => {
+    //                 renderTwoHeroes(hero1, hero2)
+    //             })
+    //             .catch( renderError );
+    //     })
+    //     .catch( renderError );
 /**
  * 
  * @param {String} id 
@@ -43,7 +76,4 @@ const findHero = ( id ) => {
 
         reject(`Hero with id ${ id } not found`);
     });
-    
-    return promise;
-
 }
