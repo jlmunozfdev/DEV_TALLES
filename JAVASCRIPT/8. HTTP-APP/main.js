a3c579d2b9feeabca45b8cabe1b88a67ceb63ca0
jlmunozfdev <jlmunozf.dev@gmail.com>
2023-01-02 23:16:48 -0500

    Update 8: Breakingbad

--- JAVASCRIPT/8. HTTP-APP/main.js
+++ JAVASCRIPT/8. HTTP-APP/main.js
@@ -1,6 +1,6 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import javascriptLogo from './javascript.svg';
+import { BreakingbadApp } from './src/breakingbad/breaking-bad-app';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -10,14 +10,16 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1 id="app-title">Hello Vite!</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+    
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+   
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+const element = document.querySelector('.card');
+
+BreakingbadApp( element );
+
+
